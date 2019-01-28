9377e4a2c605345201d317b3ae857129dfa556b3
darshanlsagar <darshanlsagar@gmail.com>
2019-01-28 16:25:51 +0530

    Append, Prepend, User hook addition
    Append, Prepend, User hook addition for the chunk id generation

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict'
+const crypto = require('crypto')
+const _path = require('path')
+
+function customChunkIds(options = {}) {
+  this.options = Object.assign(
+    {
+      append: "",
+      prepend: "",
+      entryModules: true,
+      nonEntryModules: true,
+      vendorModules: true,
+      startingCount: 0,
+      hash: false   //// Yet to be implemented
+    },
+    options
+  )
+}
+
+customChunkIds.prototype.apply = function(compiler) {
+  let options = this.options
+  compiler.hooks.compilation.tap('customChunkIds', function(compilation) {
+    compilation.hooks.beforeChunkIds.tap('customChunkIds', function(chunks) {
+      let count = options.startingCount;
+      chunks.forEach(chunk => {
+        if(options.idMaker){
+          options.idMaker(chunks);
+        } else{
+          if(options.entryModules && chunk.entryModule){
+            chunk.id = options.prepend+count+options.append;
+            count++;
+          } else if(options.vendorModules && chunk.name.startsWith("vendors~")){
+            chunk.id = options.prepend+count+options.append;
+            count++;
+          } else if(options.nonEntryModules){
+            chunk.id = options.prepend+count+options.append;
+            count++;
+          }
+          console.log("Plugin Generated Chunk Id ------------------------->"+chunk.id)
+        }
+      })
+    })
+  })
+}
+
+module.exports = customChunkIds
